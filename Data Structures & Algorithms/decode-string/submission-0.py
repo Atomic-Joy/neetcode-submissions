67cb4d892ee64823714011d85b206ae41180c195
Joy Mondal <joymondal2022@vitbhopal.ac.in>
2026-08-16 11:07:59 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack, num, cur = [], 0, ""
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == '[':
+                stack.append((cur, num))
+                cur, num = "", 0
+            elif ch == ']':
+                prev, k = stack.pop()
+                cur = prev + cur * k
+            else:
+                cur += ch
+        return cur
