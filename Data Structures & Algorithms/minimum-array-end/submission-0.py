cd03f8d95a77a01009a8f0f86799cba86c4b642c
Joy Mondal <joymondal2022@vitbhopal.ac.in>
2026-09-18 13:33:24 +0530

    Add: minimum-array-end - submission-0

---
+++ Data Structures & Algorithms/minimum-array-end/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        n -= 1
+        ans, bit = x, 0
+        while n:
+            if (x & (1 << bit)) == 0:
+                if n & 1:
+                    ans |= 1 << bit
+                n >>= 1
+            bit += 1
+        return ans
